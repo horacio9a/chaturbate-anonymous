3e954eb7426648d10d4fe174db4352338aa22424
horacio9a <horacio9a@gmail.com>
2017-08-02 11:50:11 +0200

    v.1.0.2 Batch script for easier scripts start-up - added cbffrts.py

--- cba.bat
+++ cba.bat
@@ -2,14 +2,15 @@
 SETLOCAL EnableDelayedExpansion
 :START
 ECHO.
-SET /P MODE=EXIT(5) CBFFR(4) CBR(3) CBM(2) CBFF(1) CB(0)(ENTER)(%MODE%): 
+SET /P MODE=EXIT(6) CBFFRTS(5) CBFFR(4) CBR(3) CBM(2) CBFF(1) CB(0)(ENTER)(%MODE%): 
 IF "%MODE%"=="" GOTO CB
 IF "%MODE%"=="0" GOTO CB
 IF "%MODE%"=="1" GOTO CBFF
 IF "%MODE%"=="2" GOTO CBM
 IF "%MODE%"=="3" GOTO CBR
 IF "%MODE%"=="4" GOTO CBFFR
-IF "%MODE%"=="5" GOTO EXIT
+IF "%MODE%"=="5" GOTO CBFFRTS
+IF "%MODE%"=="6" GOTO EXIT
 :CB
 ECHO.
 CLS && ECHO #################################################
@@ -110,6 +111,38 @@ cd -cba-py
 python cbffr.py %MODEL%
 TIMEOUT 5
 GOTO CBFFR_
+:CBFFRTS
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/-cba-py/CB_Model.txt) DO (
+SET /A n=n+1
+SET _fav!n!=%%A
+ECHO !n! %%A
+)
+ECHO.
+SET /P MODEL=Choose CB MODEL Name (%M%:%MODEL%): 
+FOR /L %%f IN (1,1,!n!) DO (
+IF /I '%MODEL%'=='%%f' SET M=%%f
+)
+SET n=0
+FOR /F "tokens=*" %%A IN (C:/-cba-py/CB_Model.txt) DO (
+SET /A n=n+1
+IF !n!==%M% SET MODEL=%%A
+)
+ECHO.
+SET MODELNAME=%MODEL% #####################################
+SET _MODEL_=%MODELNAME:~0,33%
+:CBFFRTS_
+ECHO.
+CLS && ECHO #################################################
+ECHO ### CBFFRTS ### R E C O R D I N G ###############
+ECHO ############### %_MODEL_%
+ECHO #################################################
+cd C:/
+COLOR 0F
+cd -cba-py
+python cbffrts.py %MODEL%
+TIMEOUT 5
+GOTO CBFFRTS_
 :EXIT
 GOTO :EOF
 ENDLOCAL
