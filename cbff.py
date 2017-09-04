a9449c4853227f1a8da641d46b0afbcaadd6e0a4
horacio9a <horacio9a@gmail.com>
2017-09-04 15:22:40 +0200

    v.1.0.4 ffmpeg & ffplay chaturbate-anonymous script (flv format)

--- cbff.py
+++ cbff.py
@@ -1,4 +1,4 @@
-# Chaturbate FFPLAY/FFMPEG Anonymous Freechat Recorder v.1.0.3 by horacio9a for Python 2.7.13
+# Chaturbate FFPLAY/FFMPEG Anonymous Freechat Recorder v.1.0.4 by horacio9a for Python 2.7.13
 
 import sys, os, urllib, urllib3, ssl, re, time, datetime, requests, random, command
 urllib3.disable_warnings()
@@ -12,27 +12,24 @@ config.read('config.cfg')
 
 init()
 print
-print(colored(" => START <= ", 'yellow', 'on_blue'))
+print(colored(" => START <=", "yellow", "on_blue"))
 print
 
 while True:
      try:
          modellist = open(config.get('files', 'model_list'),'r')
          for (num,value) in enumerate(modellist):
-            print ' =>',(num+1),value[:-1]
+            print " =>",(num+1),value[:-1]
          print
-         mn = int(raw_input(colored(" => Select CB Model URL => ", 'yellow', 'on_blue')))
+         mn = int(raw_input(colored(" => Select CB Model => ", "yellow", "on_blue")))
          print
          break
      except ValueError:
          print
-         print(colored(" => Input must be a number <= ", 'yellow', 'on_red'))
+         print(colored(" => Input must be a number <=", "yellow", "on_red"))
          print
-model_url = open(config.get('files', 'model_list'), 'r').readlines()[mn-1][:-1]
-model0 = model_url.split('://')[1]
-model1 = model0.split('/')[1]
-model = re.sub('/', '', model1)
-print (colored(' => Selected CB Model => {} <=', 'yellow', 'on_blue')).format(model)
+model = open(config.get('files', 'model_list'), 'r').readlines()[mn-1][:-1]
+print (colored(" => Selected CB Model => {} <=", "yellow", "on_blue")).format(model)
 print
 
 url ='https://chaturbate.com/{}/'.format(model)
@@ -46,21 +43,27 @@ if "HTTP 404" not in dec:
  pwd = pwd0.split("'")[0]
 
  if "currently offline" not in dec:
-   hlsurl0 = dec.split("source src='")[1]
-   hlsurl1 = hlsurl0.split("'")[0]
+  hlsurl0 = dec.split("source src='")[1]
+  hlsurl1 = hlsurl0.split("'")[0]
+
+  if len(hlsurl1) > 410:
+   print(colored(" => TRY AGAIN <=", "yellow","on_blue"))
+   sys.exit()
+  else:
+   pass
 
    if len(hlsurl1) > 1:
       hlsurl2 = hlsurl1.split('&amp')[0]
       hlsurl = re.sub('_fast_', '_', hlsurl2)
-      print (colored(' => HLS URL => {} <=', 'yellow', 'on_blue')).format(hlsurl)
+      print (colored(" => HLS URL => {} <=", "yellow", "on_blue")).format(hlsurl)
 
       while True:
            try:
                print
-               mode = int(raw_input(colored(" => Select mode (1) REC or (0) PLAY => ", 'yellow', 'on_blue')))
+               mode = int(raw_input(colored(" => Select mode (1) REC or (0) PLAY => ", "yellow", "on_blue")))
                break
            except ValueError:
-               print(colored("\n => Input must be a number <= ", 'yellow', 'on_red'))
+               print(colored("\n => Input must be a number <=", "yellow", "on_red"))
       if mode == 0:
               mod = 'PLAY'
       if mode == 1:
@@ -68,7 +71,7 @@ if "HTTP 404" not in dec:
       else:
               mod = 'PLAY'
       print
-      print (colored(' => Selected Mode => {} <=', 'yellow', 'on_blue')).format(mod)
+      print (colored(" => Selected Mode => {} <=", "yellow", "on_blue")).format(mod)
 
       timestamp = str(time.strftime("%d%m%Y-%H%M%S"))
       path = config.get('folders', 'output_folder')
@@ -77,45 +80,42 @@ if "HTTP 404" not in dec:
 
       if mod == 'PLAY':
          print
-         print (colored(' => Start ffplay PLAY => {} <=', 'white', 'on_magenta')).format(filename)
+         print (colored(" => Start ffplay => PLAY => {} <=", "white", "on_magenta")).format(filename)
          command = ('ffplay -hide_banner -loglevel panic -i {} -infbuf -autoexit -x 640 -y 480 -window_title "{} * {}"'.format(hlsurl,filename,mn))
          os.system(command)
-         time.sleep(1)    # pause 1 second
-         print(colored(" => END <= ", 'yellow','on_blue'))
-         sys.exit()
 
       if mod == 'REC':
          print
-         print (colored(' => Start ffmpeg RECORD => {} <=', 'white', 'on_red')).format(filename)
+         print (colored(" => Start ffmpeg => RECORD => {} <=", "white", "on_red")).format(filename)
          command = ('ffmpeg -hide_banner -loglevel panic -i {} -c:v copy -c:a aac -b:a 160k -f flv {}'.format(hlsurl,pf))
          os.system(command)
          time.sleep(1)    # pause 1 second
-         print(colored(" => END <= ", 'yellow','on_blue'))
+         print(colored(" => END <=", "yellow","on_blue"))
          sys.exit()
 
       else:
          print
          time.sleep(1)    # pause 1 second
-         print(colored(" => END <= ", 'yellow','on_blue'))
+         print(colored(" => END <=", "yellow","on_blue"))
          sys.exit()
 
    else:
-      print(colored(" => Model is PVT/HIDDEN or AWAY ", 'yellow','on_red'))
+      print(colored(" => Model is PVT/HIDDEN or AWAY <=", "yellow","on_red"))
       print
       time.sleep(1)    # pause 1 second
-      print(colored(" => END <= ", 'yellow','on_blue'))
+      print(colored(" => END <=", "yellow","on_blue"))
       sys.exit()
 
  else:
-   print(colored(" => Model is OFFLINE <= ", 'yellow','on_red'))
+   print(colored(" => Model is OFFLINE <=", "yellow","on_red"))
    print
    time.sleep(1)    # pause 1 second
-   print(colored(" => END <= ", 'yellow','on_blue'))
+   print(colored(" => END <=", "yellow","on_blue"))
    sys.exit()
 
 else:
-   print(colored(" => Page Not Found <= ", 'yellow','on_red'))
+   print(colored(" => Page Not Found <=", 'yellow','on_red'))
    print
-   print(colored(" => Waiting for 3 seconds <= ", 'yellow','on_blue'))
-   time.sleep(3)    # pause 3 second
+   time.sleep(1)    # pause 1 second
+   print(colored(" => END <=", "yellow","on_blue"))
    sys.exit()
