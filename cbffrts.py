a362321ca8a66c618c305c3d1de8acd26ad65441
horacio9a <horacio9a@gmail.com>
2017-09-04 15:26:04 +0200

    v.1.0.4 Remote ffmpeg 24/7 chaturbate-anonymous script (ts format)

--- cbffrts.py
+++ cbffrts.py
@@ -1,4 +1,4 @@
-# Chaturbate Remote FFPLAY/FFMPEG TS  Anonymous Freechat Recorder v.1.0.3 by horacio9a for Python 2.7.13
+# Chaturbate Remote FFPLAY/FFMPEG TS  Anonymous Freechat Recorder v.1.0.4 by horacio9a for Python 2.7.13
 
 import sys, os, urllib, urllib3, ssl, re, time, datetime, requests, random, command
 urllib3.disable_warnings()
@@ -11,7 +11,8 @@ config = ConfigParser.ConfigParser()
 config.read('config.cfg')
 
 init()
-print(colored("\n => START <= ", 'yellow', 'on_blue'))
+print
+print(colored(" => START <=", "yellow", "on_blue"))
 print
 
 if __name__=='__main__':
@@ -29,8 +30,14 @@ if "HTTP 404" not in dec:
  pwd = pwd0.split("'")[0]
 
  if "currently offline" not in dec:
-   hlsurl0 = dec.split("source src='")[1]
-   hlsurl1 = hlsurl0.split("'")[0]
+  hlsurl0 = dec.split("source src='")[1]
+  hlsurl1 = hlsurl0.split("'")[0]
+
+  if len(hlsurl1) > 410:
+   print(colored(" => TRY AGAIN <=", "yellow","on_blue"))
+   sys.exit()
+  else:
+   pass
 
    if len(hlsurl1) > 1:
       hlsurl2 = hlsurl1.split('&amp')[0]
@@ -43,36 +50,37 @@ if "HTTP 404" not in dec:
 
       bg0 = dec.split("gender: '")[1]
       bg = bg0.split("'")[0]
-      print (colored(' => INFO => HLS_URL: ({}) * BG: ({}) <= ', 'white', 'on_blue')).format(urlf,bg)
+      print (colored(" => INFO => HLS_URL: ({}) * BG: ({}) <= ", "white", "on_blue")).format(urlf,bg)
       timestamp = str(time.strftime("%d%m%Y-%H%M%S"))
       path = config.get('folders', 'output_folder')
       filename = model + '_CB_' + timestamp + '.ts'
       pf = (path + filename)
 
       print
-      print (colored(' => Start ffmpeg => RECORD => {} <=', 'white', 'on_red')).format(filename)
+      print (colored(" => Start ffmpeg => RECORD => {} <=", "white", "on_red")).format(filename)
       command = ('ffmpeg -hide_banner -loglevel panic -i {} -c copy -vsync 2 -r 60 -b:v 500k {}'.format(hlsurl,pf))
       os.system(command)
-      print(colored("\n => END <= ", 'yellow','on_blue'))
+      time.sleep(1)    # pause 1 second
+      print(colored(" => END <=", "yellow","on_blue"))
       sys.exit()
 
    else:
-      print(colored(" => Model is PVT/HIDDEN or AWAY\n", 'yellow','on_red'))
-      print(colored(" => Waiting for 60 seconds <= ", 'yellow','on_blue'))
-      time.sleep(60)    # pause 60 second
-      print(colored(" => END <= ", 'yellow','on_blue'))
+      print(colored(" => Model is PVT/HIDDEN or AWAY <=", "yellow","on_red"))
+      print
+      time.sleep(1)    # pause 1 second
+      print(colored(" => END <=", "yellow","on_blue"))
       sys.exit()
 
  else:
-   print(colored(" => Model is OFFLINE <=\n", 'yellow','on_red'))
-   print(colored(" => Waiting for 60 seconds <= ", 'yellow','on_blue'))
-   time.sleep(60)    # pause 60 second
+   print(colored(" => Model is OFFLINE <=", "yellow","on_red"))
+   print
+   time.sleep(1)    # pause 1 second
+   print(colored(" => END <=", "yellow","on_blue"))
    sys.exit()
 
 else:
-   print(colored(" => Page Not Found <= ", 'yellow','on_red'))
+   print(colored(" => Page Not Found <=", "yellow","on_red"))
    print
-   print(colored(" => Waiting for 60 seconds <= ", 'yellow','on_blue'))
-   print
-   time.sleep(60)    # pause 60 second
+   time.sleep(1)    # pause 1 second
+   print(colored(" => END <=", "yellow","on_blue"))
    sys.exit()
