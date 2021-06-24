e7da1887782777ffd884abe4bd9786cae2efecfe
horacio9a <horacio9a@gmail.com>
2021-06-24 10:50:11 +0200

    v.2.0.0 FFMPEG 24/7 Remote Script for Python39

--- cbffr3.py
+++ cbffr3.py
@@ -1,7 +1,7 @@
-# Chaturbate FFMPEG Remote Anonymous Freechat Recorder v.1.1.2 by horacio9a for Python 3.9.1
+# Chaturbate FFMPEG Remote Anonymous Freechat Recorder v.2.0.0 by horacio9a for Python 3.9.1
 # coding: utf-8
 
-import sys, os, urllib, urllib3, ssl, re, time, datetime, requests, random, command
+import sys, os, urllib, urllib3, ssl, re, time, datetime, command
 urllib3.disable_warnings()
 from urllib3 import PoolManager
 from urllib.parse import quote
@@ -9,8 +9,8 @@ from urllib.parse import unquote
 from colorama import init, Fore, Back, Style
 from termcolor import colored
 import configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
+Config = configparser.ConfigParser()
+Config.read('config3.ini')
 
 init()
 print()
@@ -37,7 +37,7 @@ if 'HTTP 404' not in dec:
   print(colored(' => END <=', 'white','on_blue'))
   sys.exit()
 
- if 'u0022offline' not in dec:
+ if 'u0022offline\\u0022' not in dec:
    status0 = dec.split('status\\u0022: ')[1]
    status1 = status0.split(', \\u0022room')[0]
    status = status1.replace('\\u0022', '')
@@ -79,15 +79,14 @@ if 'HTTP 404' not in dec:
       print ((colored(' => INFO => Real Name: ({}) * Location: ({}) * Age: ({}) * Sex: ({}) <=', 'white', 'on_blue')).format(rn,loc,age,bg))
 
       timestamp = str(time.strftime('%d%m%Y-%H%M%S'))
-      path = config.get('folders', 'output_folder')
+      path = Config.get('folders', 'output_folder')
       filename = model + '_CB_' + timestamp + '.flv'
       pf = (path + filename)
-      ffmpeg = config.get('files', 'ffmpeg')
-
+      ffmpeg = Config.get('files', 'ffmpeg')
+	  
       print()
-      print((colored(' => FF-FLV-REC => {} <=', 'white', 'on_red')).format(filename))
-      print()
-      command = '{} -hide_banner -loglevel panic -i {} -c:v copy -c:a aac -b:a 128k {}'.format(ffmpeg,hlsurl,pf)
+      print ((colored(' => FFMPEG-REC => {} <=', 'white', 'on_red')).format(filename))
+      command = ('{} -hide_banner -loglevel panic -i {} -c:v copy -c:a aac -b:a 128k {}'.format(ffmpeg,hlsurl,pf))
       os.system(command)
       print()
       print(colored(' => END <=', 'white','on_blue'))
