89e237dc680af707af68487957981e3795dd2b79
horacio9a <horacio9a@gmail.com>
2021-06-24 10:48:12 +0200

    v.2.0.0 Chaturbate Anonymous Freechat Recorder for Python39

--- cb3.py
+++ cb3.py
@@ -1,7 +1,7 @@
-# Chaturbate FFPLAY/STREAMLINK/FFMPEG/YTDL Anonymous Freechat Recorder v.1.1.2 by horacio9a for Python 3.9.1
+# Chaturbate FFPLAY/STREAMLINK/FFMPEG/YTDL Anonymous Freechat Recorder v.2.0.0 by horacio9a for Python 3.9.1
 # coding: utf-8
 
-import sys, os, urllib, urllib3, ssl, re, time, datetime, requests, random, command
+import sys, os, urllib, urllib3, ssl, re, time, datetime, command
 urllib3.disable_warnings()
 from urllib3 import PoolManager
 from urllib.parse import quote
@@ -9,8 +9,8 @@ from urllib.parse import unquote
 from colorama import init, Fore, Back, Style
 from termcolor import colored
 import configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
+Config = configparser.ConfigParser()
+Config.read('config3.ini')
 
 init()
 print()
@@ -19,7 +19,7 @@ print()
 
 while True:
    try:
-      mode = int(input(' => Select => CB_Online_All(2) CB_Online_Wanted(1) CB_Wanted(0) => '))
+      mode = int(input(colored(' => Select => CB_Online_All(2) CB_Online_Wanted(1) CB_Wanted(0) => ', 'white', 'on_blue')))
       print()
       break
    except ValueError:
@@ -37,13 +37,13 @@ if mode == 2:
 if mod == 'CBW':
   while True:
      try:
-        modellist = open(config.get('files', 'wanted_model_list'),'r')
+        modellist = open(Config.get('files', 'wanted_model_list'),'r')
         for (num,value) in enumerate(modellist):
            print(' =>',(num+1),value[:-1])
         print()
-        mn = int(input(' => Select CB Wanted Model => '))
+        mn = int(input(colored(' => Select CB Wanted Model => ', 'white', 'on_blue')))
         print()
-        nr_lines = sum(1 for line in open(config.get('files', 'wanted_model_list')))
+        nr_lines = sum(1 for line in open(Config.get('files', 'wanted_model_list')))
         if mn > nr_lines:
            print(colored(' => Too big number <=', 'white', 'on_red'))
            print()
@@ -52,20 +52,20 @@ if mod == 'CBW':
         break
      except ValueError:
         print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-  model = open(config.get('files', 'wanted_model_list'), 'r').readlines()[mn-1][:-1]
+  model = open(Config.get('files', 'wanted_model_list'), 'r').readlines()[mn-1][:-1]
   print ((colored(' => Selected CB Wanted Model => {} <=', 'white', 'on_blue')).format(model))
   print()
 
 if mod == 'CBOW':
   while True:
      try:
-        modellist = open(config.get('files', 'online_wanted_model_list'),'r')
+        modellist = open(Config.get('files', 'online_wanted_model_list'),'r')
         for (num,value) in enumerate(modellist):
            print(' =>',(num+1),value[:-1])
         print()
-        mn = int(input(' => Select CB Online Wanted Model => '))
+        mn = int(input(colored(' => Select CB Online Wanted Model => ', 'white', 'on_blue')))
         print()
-        nr_lines = sum(1 for line in open(config.get('files', 'online_wanted_model_list')))
+        nr_lines = sum(1 for line in open(Config.get('files', 'online_wanted_model_list')))
         if mn > nr_lines:
            print(colored(' => Too big number <=', 'white', 'on_red'))
            print()
@@ -74,14 +74,14 @@ if mod == 'CBOW':
         break
      except ValueError:
         print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-  model = open(config.get('files', 'online_wanted_model_list'), 'r').readlines()[mn-1][:-1]
+  model = open(Config.get('files', 'online_wanted_model_list'), 'r').readlines()[mn-1][:-1]
   print ((colored(' => Selected CB Online Wanted Model => {} <=', 'white', 'on_blue')).format(model))
   print()
 
 if mod == 'CBOA':
   while True:
      try:
-        cboa = int(input(' => Select => <500(0) <1000(1) <1500(2) <2000(3) <2500(4) <3000(5) <3500(6) => '))
+        cboa = int(input(colored(' => Select => <500(0) <1000(1) <1500(2) <2000(3) <2500(4) <3000(5) <3500(6) => ', 'white', 'on_blue')))
         print()
         break
      except ValueError:
@@ -106,15 +106,15 @@ if mod == 'CBOA':
   if oa == 'OA500':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (500, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Models => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -123,22 +123,22 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA1000':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (1000, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -147,22 +147,22 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA1500':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (1500, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -171,22 +171,22 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA2000':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (2000, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -195,22 +195,22 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA2500':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (2500, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -219,22 +219,22 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA3000':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (3000, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
-          nr_lines = sum(1 for line in open(config.get('files', 'online_all_model_list')))
+          nr_lines = sum(1 for line in open(Config.get('files', 'online_all_model_list')))
           if mn > nr_lines:
              print(colored(' => Too big number <=', 'white', 'on_red'))
              print()
@@ -243,25 +243,25 @@ if mod == 'CBOA':
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
   if oa == 'OA3500':
     while True:
        try:
-          modellist = open(config.get('files', 'online_all_model_list'),'r')
+          modellist = open(Config.get('files', 'online_all_model_list'),'r')
           for (num, value) in enumerate(modellist):
             if num in range (3500, 5000):
               break
             print(' =>',(num+1),value[:-1])
           print()
-          mn = int(input(' => Select CB Online All Model => '))
+          mn = int(input(colored(' => Select CB Online All Models => ', 'white', 'on_blue')))
           print()
           break
        except ValueError:
           print(colored('\n => Input must be a number <=\n', 'white', 'on_red'))
-    model = open(config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
+    model = open(Config.get('files', 'online_all_model_list'), 'r').readlines()[mn-1][:-1]
     print ((colored(' => Selected CB Online All Model => {} <=', 'white', 'on_blue')).format(model))
     print()
 
@@ -281,7 +281,7 @@ if 'HTTP 404' not in dec:
   print(colored(' => END <=', 'white','on_blue'))
   sys.exit()
  
- if 'u0022offline' not in dec:
+ if 'u0022offline\\u0022' not in dec:
    status0 = dec.split('status\\u0022: ')[1]
    status1 = status0.split(', \\u0022room')[0]
    status = status1.replace('\\u0022', '')
@@ -324,31 +324,33 @@ if 'HTTP 404' not in dec:
       while True:
          try:
             print()
-            prog = int(input(' => Mode => Exit(5) => URL(4) => YTDL(3) => SL(2) => FF-FLV(1) => FF-VIEW(0) => '))
+            mode = int(input(colored(' => Mode => Exit(6) - URL(5) - YTDL(4) - LS(3) - SL(2) - FFMPEG(1) - PLAYER(0) => ', 'white', 'on_green')))
             break
          except ValueError:
             print()
             print(colored(' => Input must be a number <=', 'white', 'on_red'))
-      if prog > 6:
+      if mode > 6:
          print()
          print(colored(' => Too big number <=', 'white', 'on_red'))
-         sys.exit()
-      if prog == 0:
-         prg = 'FF-VIEW'
-      if prog == 1:
-         prg = 'FF-FLV'
-      if prog == 2:
-         prg = 'SL'
-      if prog == 3:
-         prg = 'YTDL'
-      if prog == 4:
-         prg = 'URL'
-      if prog == 5:
-         prg = 'EXIT'
+         mod = 'EXIT'
+      if mode == 0:
+         mod = 'PLAYER'
+      if mode == 1:
+         mod = 'FFMPEG'
+      if mode == 2:
+         mod = 'SL'
+      if mode == 3:
+         mod = 'LS'
+      if mode == 4:
+         mod = 'YTDL'
+      if mode == 5:
+         mod = 'URL'
+      if mode == 6:
+         mod = 'EXIT'
 
       timestamp = str(time.strftime('%d%m%Y-%H%M%S'))
-      path = config.get('folders', 'output_folder')
-      filename = model + '_CB_' + timestamp
+      path = Config.get('folders', 'output_folder')
+      fn = model + '_CB_' + timestamp
       fn1 = model + '_CB_' + timestamp + '.flv'
       fn2 = model + '_CB_' + timestamp + '.mp4'
       fn3 = model + '_CB_' + timestamp + '.ts'
@@ -357,19 +359,22 @@ if 'HTTP 404' not in dec:
       pf2 = (path + fn2)
       pf3 = (path + fn3)
       pf4 = (path + fn4)
-      ffmpeg = config.get('files', 'ffmpeg')
-      ffplay = config.get('files', 'ffplay')
-      streamlink = config.get('files', 'streamlink')
-      youtube = config.get('files', 'youtube')
+      player = Config.get('files', 'player')
+      ffmpeg = Config.get('files', 'ffmpeg')
+      youtube = Config.get('files', 'youtube')
+      streamlink = Config.get('files', 'streamlink')
+      livestreamer = Config.get('files', 'livestreamer')
 
-      if prg == 'FF-VIEW':
+      if mod == 'PLAYER':
          print()
-         print ((colored(' => FF-PLAY => {} <=', 'white', 'on_magenta')).format(filename))
-         command = ('{} -hide_banner -loglevel panic -i {} -vf scale=640:480 -infbuf -framedrop -autoexit -window_title "{} * {} {}"'.format(ffplay,hlsurl,filename,mn,mod))
+         print ((colored(' => PLAYER => {} <=', 'white', 'on_magenta')).format(fn))
+         print()
+         command = ('{} -p {} {} best'.format(streamlink, player, hlsurl))
          os.system(command)
          while True:
             try:
-               prog = int(input(' => Mode => URL(4) => YTDL(3) => SL(2) => FF-FLV(1) => Exit(0) => '))
+               print()
+               prog = int(input(colored(' => Mode => URL(5) - YTDL(4) - LS(3) - SL(2) - FFMPEG(1) - Exit(0) => ', 'white', 'on_green')))
                break
             except ValueError:
                print()
@@ -377,29 +382,31 @@ if 'HTTP 404' not in dec:
          if prog > 5:
             print()
             print(colored(' => Too big number <=', 'white', 'on_red'))
-            prg = 'EXIT'
+            mod = 'EXIT'
          if prog == 0:
-            prg = 'EXIT'
+            mod = 'EXIT'
          if prog == 1:
-            prg = 'FF-FLV'
+            mod = 'FFMPEG'
          if prog == 2:
-            prg = 'SL'
+            mod = 'SL'
          if prog == 3:
-            prg = 'YTDL'
+            mod = 'LS'
          if prog == 4:
-            prg = 'URL'
+            mod = 'YTDL'
+         if prog == 5:
+            mod = 'URL'
 
-      if prg == 'FF-FLV':
+      if mod == 'FFMPEG':
          print()
-         print ((colored(' => FF-FLV-REC => {} <=', 'white', 'on_red')).format(fn1))
+         print ((colored(' => FFMPEG-REC => {} <=', 'white', 'on_red')).format(fn1))
          command = '{} -hide_banner -loglevel panic -i {} -c:v copy -c:a aac -b:a 128k {}'.format(ffmpeg,hlsurl,pf1)
          os.system(command)
          print(colored(' => END <=', 'white','on_blue'))
          sys.exit()
 
-      if prg == 'SL':
+      if mod == 'SL':
          print()
-         print ((colored(' => SL-REC >>> {}.mp4 <<<', 'white', 'on_red')).format(filename))
+         print ((colored(' => SL-REC => {}  (  Size  @   Speed   ) <=', 'white', 'on_red')).format(fn2))
          print()
          command = ('{} hls://{} best -Q --hls-live-edge 1 --hls-playlist-reload-attempts 9 --hls-segment-threads 3 --hls-segment-timeout 5.0 --hls-timeout 20.0 -o {}'.format(streamlink,hlsurl,pf2))
          os.system(command)
@@ -407,9 +414,19 @@ if 'HTTP 404' not in dec:
          print(colored(' => END <=', 'white','on_blue'))
          sys.exit()
 
-      if prg == 'YTDL':
+      if mod == 'LS':
          print()
-         print ((colored(' => YTDL-REC => {}.ts <=', 'white', 'on_red')).format(filename))
+         print ((colored(' => LS-REC => {}  (  Size  @   Speed   ) <=', 'white', 'on_red')).format(fn2))
+         print()
+         command = ('{} hlsvariant://{} best -Q -o {}'.format(livestreamer,hlsurl,pf2))
+         os.system(command)
+         print()
+         print(colored(' => END <= ', 'white','on_blue'))
+         sys.exit()
+
+      if mod == 'YTDL':
+         print()
+         print ((colored(' => YTDL-REC => {}.ts <=', 'white', 'on_red')).format(fn))
          print()
          command = ('{} -i --geo-bypass --hls-use-mpegts --no-part -q --no-warnings --no-check-certificate {} -o {}'.format(youtube,hlsurl,pf3))
          os.system(command)
@@ -417,7 +434,7 @@ if 'HTTP 404' not in dec:
          print(colored(' => END <=', 'white','on_blue'))
          sys.exit()
 
-      if prg == 'URL':
+      if mod == 'URL':
          print()
          print((colored(' => URL => {} <=', 'white', 'on_green')).format(fn4))
          file=open(pf4,'w')
@@ -427,7 +444,7 @@ if 'HTTP 404' not in dec:
          print(colored(' => END <=', 'white','on_blue'))
          sys.exit()
 
-      if prg == 'EXIT':
+      if mod == 'EXIT':
          print()
          print(colored(' => END <=', 'white','on_blue'))
          sys.exit()
