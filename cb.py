3e3697427dbf6de667fcf41f7aa42390f882686c
horacio9a <horacio9a@gmail.com>
2021-06-25 09:10:19 +0200

    v.2.0.0 Chaturbate Multimode Anonymous Freechat Recorder for Python27

--- cb.py
+++ cb.py
@@ -1,4 +1,4 @@
-# Chaturbate FFPLAY/STREAMLINK/FFMPEG/YTDL Anonymous Freechat Recorder v.2.0.0 by horacio9a for Python 2.7.18
+# Chaturbate FFMPEG/STREAMLINK/LIVESTREAM/YTDL Anonymous Freechat Recorder v.2.0.0 by horacio9a for Python 2.7.18
 # coding: utf-8
 
 import sys, os, urllib, urllib3, ssl, re, time, datetime, command
